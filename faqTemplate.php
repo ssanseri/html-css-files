5678310b9a5a6a887ef8ef676fc916ca8f6ae50b
ssanseri <samuel.sanseri@gmail.com>
2016-08-12 18:47:54 -0700

    Added code from HTML/CSS tutorials
    publishing HTML/CSS working code

---
+++ faqTemplate.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Template Name: FAQ Page
+ */
+
+get_header(); 
+
+?>
+
+<html>
+<body>
+
+<p>Oh, you didn't have any questions?  Well, ask me no questions and I'll tell you no lies!</p>
+
+</body>
+</html>
